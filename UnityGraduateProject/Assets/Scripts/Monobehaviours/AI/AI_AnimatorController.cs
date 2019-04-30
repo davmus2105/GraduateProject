f15cd77194b68eaf8bca83caa041496d00e50b05
Musienko David <david.musienko@gmail.com>
2019-04-30 15:46:14 +0300

    ai_controller_changes_002

--- UnityGraduateProject/Assets/Scripts/Monobehaviours/AI/AI_AnimatorController.cs
+++ UnityGraduateProject/Assets/Scripts/Monobehaviours/AI/AI_AnimatorController.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class AI_AnimatorController : MonoBehaviour
 {
-    
+    #region Fields and properties
+    AI_CharacterController aicontroller;
+    #endregion
+    #region Monobehaviours methods
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+    #region Methods
+    #endregion
 }
