d274fb1da1820f8129e25507fe2714c8950cc044
NazMarch <nazaran237@gmail.com>
2019-05-15 02:05:19 +0300

    testing ChangerDay

--- UnityGraduateProject/Assets/Scripts/Managers/ChangerDay.cs
+++ UnityGraduateProject/Assets/Scripts/Managers/ChangerDay.cs
@@ -6,13 +6,15 @@ public class ChangerDay : MonoBehaviour
 {
     public float Dayspeed = 0.01f;
     //public float ScrollY = 0f;
+    public GameObject AllTimeDayPref;
     public GameObject Sun;
     public GameObject Moon;
     public GameObject clouds;
     public GameObject sky;
     Renderer skyrend,cloudsrend;
-    //private float counter = 1f;
-    
+    private float counter = 1f;
+    public float OffsetX;
+
 
 
 
@@ -25,34 +27,36 @@ public class ChangerDay : MonoBehaviour
     }
     void Update()
     {
-       float OffsetX = Time.time * Dayspeed; // 0.5 > 0.01  time.delta = 0.02 * 0.5
+       OffsetX = Time.time * Dayspeed; // 0.5 > 0.01  time.delta = 0.02 * 0.5
         skyrend.material.mainTextureOffset = new Vector2(OffsetX, 0); //cloudsrend.material.mainTextureOffset = new Vector2(OffsetX, 0);
         cloudsrend.material.mainTextureOffset = new Vector2(OffsetX, 0);
+       
         
         //Sun.transform.Rotate(-200, speed * Time.deltaTime, 0);
         Sun.transform.RotateAround(Vector3.zero, Vector3.right, Dayspeed * 400 * Time.deltaTime);
-        Moon.transform.RotateAround(Vector3.zero, Vector3.right, Dayspeed*400 * Time.deltaTime);
+        Moon.transform.RotateAround(Vector3.zero, Vector3.right, Dayspeed * 400 * Time.deltaTime);
         //sun_moon.rotation = Quaternion.Euler(sun_moon.rotation.x -speed,0,0);
-      
+
         
-        /*  if (counter < OffsetX)
+          if (counter < OffsetX)
         {
+            Debug.Log(Zero(OffsetX));// 51,00039
             OffsetX = Zero(OffsetX);
             skyrend.material.mainTextureOffset = new Vector2(OffsetX, 0);
             cloudsrend.material.mainTextureOffset = new Vector2(OffsetX, 0);
-            Debug.Log("Offset is");
+            Debug.Log("Offset is!!! Some changing");
         }
-        else
+        /*else
         {
             OffsetX = Time.deltaTime * Dayspeed;
         }*/
     }
-    /*public float Zero(float Value)
+    public float Zero(float Value)
     {
-        float a = Value;
-        float b = a + 4;
-        return b;
-    }*/
+        float a = 1f;
+        
+        return a;
+    }
 
 
 }
