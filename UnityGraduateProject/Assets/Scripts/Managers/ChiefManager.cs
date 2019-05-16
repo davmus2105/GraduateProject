c596b200d105387d9af453596e7d2fa9dc425b81
Musienko David <david.musienko@gmail.com>
2019-05-16 13:14:32 +0300

    ChiefManager

--- UnityGraduateProject/Assets/Scripts/Managers/ChiefManager.cs
+++ UnityGraduateProject/Assets/Scripts/Managers/ChiefManager.cs
@@ -1,35 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ChiefManager : MonoBehaviour
 {
     [Tooltip("Managers that will be added on start")]
     // managers to add on [SETUP] when game is started
-    public List<string> managerTypes;
+    public List<string> managers;
+    [Space(25)]
+    [SerializeField]string filename = "managers_list.txt";
+    [SerializeField]string managerspath = "/Kernel/";
+
     private void Awake()
     {
-        if (managerTypes == null)
-            managerTypes = new List<string>();
+        if (!LoadManagersList())
+        {
+            managers = new List<string>();
+        }
+    }
+    private void OnEnable()
+    {
+        LoadManagersList();
+    }
+    private void OnDisable()
+    {
+        SaveManagersList();
     }
     void Start()
     {
-        if (managerTypes.Count > 0)
+        // ----- Managers setup -----
+        if (managers.Count > 0)
         {
             List<MonoBehaviour> managerexists = new List<MonoBehaviour>();
-            var existComp = gameObject.GetComponents<MonoBehaviour>();
-            foreach (var manager in managerTypes)
+            foreach (var manager in managers)
             {
                 var component = System.Type.GetType(manager);
-                gameObject.AddComponent(component);
+                if (gameObject.GetComponent(component) == null)
+                    gameObject.AddComponent(component);
             }
         }
+        // ---- UI SETUP ----
 
     }
 
     
-    void Update()
+    bool LoadManagersList()
     {
-        
+        string path = Application.streamingAssetsPath + managerspath + filename;
+        string[] lines = File.ReadAllLines(path);
+        if (lines == null || lines.Length == 0)
+        {
+            Debug.Log("Something has gone wrong");
+            return false;
+        }
+            
+        if (managers == null)
+            managers = new List<string>(lines.Length);
+        else
+            managers.Clear();
+        for (int i = 0; i < lines.Length; i++)
+        {
+            managers.Add(lines[i]);
+        }
+        return true;
+    }
+    void SaveManagersList()
+    {
+        if (managers == null || managers.Count == 0)
+            return;
+        string path = Application.streamingAssetsPath + managerspath + filename;
+        File.WriteAllLines(path, managers);        
     }
 }
