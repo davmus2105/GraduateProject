188298e6bceb9e42bb8df0cfeda846b63493e73d
NazMarch <nazaran237@gmail.com>
2019-05-17 23:22:42 +0300

    Map changes

--- UnityGraduateProject/Assets/Scripts/ManagerScript.cs
+++ UnityGraduateProject/Assets/Scripts/ManagerScript.cs
@@ -5,21 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class ManagerScript : MonoBehaviour
 {
+    public string loadName;
+    public string unloadName;
     public static ManagerScript Instance{set;get;}
 
     private void Awake() 
     {
         Instance = this;
-        Load("Player");
-        Load("first"); 
+        Load("Map");
+        Load("village"); 
 
     }
 
     public void Load(string sceneName)
     {
-        if(!SceneManager.GetSceneByName(sceneName).isLoaded)
-            SceneManager.LoadScene (sceneName,LoadSceneMode.Additive);
-    }
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+            {
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            }
+        }
 
     public void Unload (string sceneName)
     {
@@ -27,4 +31,25 @@ public class ManagerScript : MonoBehaviour
             SceneManager.UnloadScene(sceneName);
     
     }
+    
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (loadName != "")
+        {
+            ManagerScript.Instance.Load(loadName);
+        }
+        if (unloadName != "")
+        {
+            StartCoroutine("UnloadScene");
+        }
+
+    }
+    IEnumerator UnloadScene()
+    {
+        yield return new WaitForSeconds(.10f);
+        ManagerScript.Instance.Unload(unloadName);
+    }
+
+
 }
