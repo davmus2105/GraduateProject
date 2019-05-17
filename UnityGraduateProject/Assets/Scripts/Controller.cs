da1876ede80c799daede36feea6a92c0d0a26c39
NazMarch <nazaran237@gmail.com>
2019-05-17 12:08:05 +0300

    Add boss

---
+++ UnityGraduateProject/Assets/Scripts/Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+
+{
+    public GameObject Button_Load_Scene;
+    public GameObject Slider_of_load;
+    public Slider TheSlider;
+    // Start is called before the first frame update
+    public void LoadScene( int sceneIndex)
+    {
+        StartCoroutine(LoadAsync (sceneIndex));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        Button_Load_Scene.SetActive(false);
+        Slider_of_load.SetActive(true);
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress/.9f);
+            TheSlider.value = progress;
+            yield return null;
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityGraduateProject/Assets/Scripts/ManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerScript : MonoBehaviour
+{
+    public static ManagerScript Instance{set;get;}
+
+    private void Awake() 
+    {
+        Instance = this;
+        Load("Player");
+        Load("first"); 
+
+    }
+
+    public void Load(string sceneName)
+    {
+        if(!SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.LoadScene (sceneName,LoadSceneMode.Additive);
+    }
+
+    public void Unload (string sceneName)
+    {
+        if(SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.UnloadScene(sceneName);
+    
+    }
+}
