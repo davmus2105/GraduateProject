fbf8ae6f6de378b84f6799c871ad5c5359e982ee
Musienko David <david.musienko@gmail.com>
2019-05-17 10:33:16 +0300

    AudioManager is done

---
+++ UnityGraduateProject/Assets/Scripts/Audio/AudioCollection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Audio
+{
+    [CreateAssetMenu(fileName = "AudioCollection", menuName = "AudioCollection/Create new Audio Collection")]
+    public class AudioCollection : ScriptableObject
+    {
+        // ---------------------- Footsteps Collections ------------------------
+        public AudioClip[]
+                footsteps_gravel,
+                footsteps_grass,
+                footsteps_mud;
+        // ---------------------- Ambience Collections ------------------------
+        public AudioClip[]
+                ambience_inForest_birdsSong;
+        // ---------------------- Battle Effects Collections ------------------------
+        public AudioClip[]
+                battleCry,
+                battle_grunts;
+
+        public AudioClip
+                get_sword,
+                hide_sword;
+        // ---------------------- Background Music Collections ------------------------
+        public AudioClip[]
+                battle_music,
+                tranqul_music;
+
+    }
+}
--- UnityGraduateProject/Assets/Scripts/Extensible/ExM.cs
+++ UnityGraduateProject/Assets/Scripts/Extensible/ExM.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExM
+public static class ExM
 {
     public static void SetActiveGameObjects(GameObject[] list, bool active)
     {
@@ -15,4 +15,13 @@ public class ExM
             go.gameObject.SetActive(active);
     }
 
+    // --------- GET RANDOM ITEM -----------
+    public static T GetRandomItem<T>(this T[] list)
+    {
+        return list[Random.Range(0, list.Length - 1)];
+    }
+    public static T GetRandomItem<T>(this List<T> collection)
+    {
+        return collection[Random.Range(0, collection.Count - 1)];
+    }
 }
