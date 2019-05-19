56065fc3b4fa782d7a9398ffb39ce424d8be9a48
Musienko David <david.musienko@gmail.com>
2019-05-19 08:47:48 +0300

    trigger for teaching materials near the boat

---
+++ UnityGraduateProject/Assets/Scripts/Monobehaviours/Triggers/TriggerEvents/SceneChangeTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TES
+{
+    public class SceneChangeTrigger : TriggerEventComponent
+    {
+        public int requiredamount;
+        public override void ExecuteEvent(Collider collider = null, TriggerEventContainer container = null)
+        {
+            int result = DialogueManager.Instance.rightResult;
+            if (result == requiredamount)
+                ChangeScene();
+            else
+            {
+                Debug.Log("You have not completed your quest");
+            }
+        }
+        void ChangeScene()
+        {
+            //QuestSystem.QuestManager.Instance.CompleteQuest(#quest id);
+            // TODO:
+            // The way that scene will be changed
+            Debug.Log("Your way is finished");
+        }
+    }
+}
