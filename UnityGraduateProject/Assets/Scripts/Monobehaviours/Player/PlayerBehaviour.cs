50a5d390772b8689d41dde30b65b05167de3efb6
Musienko David <david.musienko@gmail.com>
2019-05-24 09:18:46 +0300

    map is changed

---
+++ UnityGraduateProject/Assets/Scripts/Monobehaviours/Player/PlayerBehaviour.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour, IDie
+{    
+    [SerializeField] float movespeed;
+    [SerializeField] float gravity;
+    [SerializeField] float turnSpeed;
+    [SerializeField] float rotSmooth;
+    [SerializeField] float inBattleDist;
+    [SerializeField] CharacterController charcontr;
+    float inputX, inputY, angle;
+    public bool canMove, isDead;
+    public bool isBlocking
+    {
+        get
+        {
+            return isblocking;
+        }
+        set
+        {
+            isblocking = value;
+            actor.isBlocking = isblocking;
+            animcontr.BlockAnimation(isblocking);
+        }
+    }
+    bool isblocking, in_battle;
+
+    public static PlayerBehaviour Instance;
+    private static PlayerBehaviour instance;
+    public bool inBattle
+    {
+        get { return in_battle; }
+        set
+        {
+            if (value != in_battle)
+            {
+                if (value == false)
+                {
+                    audioManager.isInBattle = false;
+                    audioManager.BackgroundChoose();
+                    in_battle = value;
+                }
+                else
+                {
+                    audioManager.isInBattle = true;
+                    audioManager.BackgroundChoose();
+                    in_battle = value;
+                }
+            }
+        }
+    }
+    PlayerAnimatorController animcontr;
+    Actor actor;
+    Transform playeModel;
+    [SerializeField] Transform charRotTarget;
+    Vector3 movevector;
+    Vector3 rotateVector;
+    Quaternion playerRot;
+    public GraduateAudio.AudioManager audioManager;
+
+    public bool isMoving;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        charcontr = GetComponent<CharacterController>();
+        playeModel = transform.Find("Model");
+        animcontr = GetComponentInChildren<PlayerAnimatorController>();
+        charRotTarget = Camera.main.transform.GetChild(0);
+        canMove = true;
+        isblocking = false;
+        inBattle = false;
+        actor = GetComponent<Actor>();
+        audioManager = GraduateAudio.AudioManager.Instance;
+        // Vars start:
+        turnSpeed = 2f;
+    }
+
+    private void Update()
+    {
+        if (canMove)
+            Moving();
+        Inputs();
+        BattleControll();
+    }
+
+    void BattleControll()
+    {
+        if (AI.AI_Manager.Instance.IsEnemyNear(transform.position, inBattleDist))
+            inBattle = true;
+        else
+            inBattle = false;
+
+
+    }
+
+    void Moving()
+    {
+        if (DialogueManager.Instance.inDialogue)
+        {
+            isMoving = false;
+            return;
+        }
+        else if (charcontr.isGrounded)
+        {
+            float speed = 0;
+            /*movevector = new Vector3(Input.GetAxis("Horizontal"), 
+                                        0f, Input.GetAxis("Vertical"));
+            movevector = transform.TransformDirection(movevector);
+            movevector *= movespeed;*/
+
+            if (Input.GetButton("Right"))
+            {
+                playeModel.rotation = Quaternion.Lerp(playeModel.rotation, Quaternion.Euler(0, playeModel.rotation.y + 90, 0), rotSmooth);
+                speed = movespeed;
+            }
+            if (Input.GetButton("Left"))
+            {
+                playeModel.rotation = Quaternion.Lerp(playeModel.rotation, Quaternion.Euler(0, playeModel.rotation.y - 90, 0), rotSmooth);
+                speed = movespeed;
+            }
+            if (Input.GetButton("Forward"))
+            {
+                playeModel.rotation = Quaternion.Lerp(playeModel.rotation, Quaternion.Euler(0, playeModel.rotation.y, 0), rotSmooth);
+                speed = movespeed;
+            }
+            if (Input.GetButton("Back"))
+            {
+                playeModel.rotation = Quaternion.Lerp(playeModel.rotation, Quaternion.Euler(0, playeModel.rotation.y + 180, 0), rotSmooth);
+                speed = movespeed;
+            }
+            if (speed != 0)
+                isMoving = true;
+            else
+                isMoving = false;
+            movevector = playeModel.TransformDirection(Vector3.forward);
+            movevector *= speed;
+        }
+        else
+        {
+            movevector.y -= gravity * Time.deltaTime;
+        }
+
+        charcontr.Move(movevector * Time.deltaTime);
+    }
+    void Inputs()
+    {
+        if (Input.GetButtonDown("Quests"))
+        {
+            HUD_Controller.Instance.ShowHideQuestPanel();
+            Debug.Log("Show quest panel");
+        }
+        if (Input.GetButtonDown("Block"))
+            isBlocking = true;
+        else if (Input.GetButtonUp("Block"))
+            isBlocking = false;
+    }
+    public void Die()
+    {
+        canMove = false;
+        isDead = true;
+        animcontr.Death();
+    }
+    public void Death()
+    {
+        EventManager.TriggerEvent("PlayerDeath");
+    }
+}
---
+++ UnityGraduateProject/Assets/Scripts/Monobehaviours/Player/Player_Animator_Controller.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using GraduateAudio;
+
+public class Player_Animator_Controller : MonoBehaviour
+{
+    Animator animator;
+    PlayerBehaviour player;
+    GameObject backplace, inhandplace;
+    Collider weaponCollider;
+    const float AFTER_DEATH_WAIT = 6f;
+    int speed_hash = Animator.StringToHash("speed");
+    int jump_hash = Animator.StringToHash("jump");
+    int arm = Animator.StringToHash("arm_disarm");
+    int slash = Animator.StringToHash("slash");
+    int block = Animator.StringToHash("block");
+    int die = Animator.StringToHash("die");
+    int[] triggers;
+    bool isArmored;
+    // ----- instances of managers to work with -----
+    HUD_Controller hudController;
+    DialogueManager dialogueManager;
+    AudioSource audioSource;
+    public AudioCollection audio_collection;
+
+    void Start()
+    {
+        triggers = new int[] { jump_hash, speed_hash, arm, slash, block, die };
+        animator = GetComponent<Animator>();
+        player = GetComponentInParent<PlayerBehaviour>();
+        backplace = transform.Find("Armature").Find("Hips").Find("LowerSpine").
+                    Find("Chest").Find("WeaponOnBackPlace").gameObject;
+        inhandplace = transform.Find("Armature").Find("Hips").Find("LowerSpine").
+                      Find("Chest").Find("ShoulderConnector.R").Find("Shoulder.R").
+                      Find("UpperArm.R").Find("LowerArm.R").
+                      Find("Hand.R").Find("WeaponInHandPlace").gameObject;
+        weaponCollider = inhandplace.GetComponentInChildren<Collider>();
+        weaponCollider.enabled = false;
+        isArmored = false;
+        inhandplace.SetActive(false);
+        // ---- Instances ----
+        hudController = HUD_Controller.Instance;
+        dialogueManager = DialogueManager.Instance;
+        audioSource = GetComponent<AudioSource>();
+        if (!audioSource)
+            audioSource = gameObject.AddComponent<AudioSource>();
+    }
+    void Update()
+    {
+        MoveAnimations();
+        KeyInputControll();
+    }
+    void MoveAnimations()
+    {
+        animator.SetFloat(speed_hash, Convert.ToInt32(player.isMoving));
+    }
+    public void JumpAnimation()
+    {
+        ResetAllTriggers();
+        animator.SetTrigger(jump_hash);
+    }
+    public void BlockAnimation(bool isBlocking)
+    {
+        ResetAllTriggers();
+        if (isBlocking)
+            SetCanMoveFalse();
+        else
+            SetCanMoveTrue();
+        animator.SetBool(block, isBlocking);
+    }
+    public void Death()
+    {
+        // Death animation consist of:            
+        // 1. Coroutine "DeathAnimation" and after a few seconds to call destroing
+        StartCoroutine("DeathAnimation");
+    }
+    void ResetAllTriggers()
+    {
+        for (int i = 0; i < triggers.Length; i++)
+        {
+            animator.ResetTrigger(triggers[i]);
+        }
+    }
+    void ChangeArmoredState()
+    {
+        ResetAllTriggers();
+        animator.SetTrigger(arm);
+    }
+    void TurnOnWeaponCollider()
+    {
+        weaponCollider.enabled = true;
+    }
+    void TurnOffWeaponCollider()
+    {
+        weaponCollider.enabled = false;
+    }
+    void KeyInputControll()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            JumpAnimation();
+        }
+        if (Input.GetButtonDown("Attack"))
+        {
+            Attack();
+        }
+        if (Input.GetButtonDown("Arm/Disarm"))
+        {
+            ArmDisarm();
+        }
+    }
+    void Attack()
+    {
+        if (dialogueManager.inDialogue || hudController.inQuestMenu || PauseMenu.GameIsPaused)
+            return;
+        if (isArmored)
+        {
+            animator.SetTrigger(slash);
+            audioSource.volume = AudioManager.battleEffects_volume;
+            audioSource.clip = audio_collection.battle_grunts.GetRandomItem();
+            audioSource.Play();
+        }
+        else
+        {
+            hudController.ShowInfoMessage("You are not armored");
+        }
+    }
+
+    void ArmDisarm()
+    {
+        isArmored = !isArmored;
+        weaponCollider = inhandplace.GetComponentInChildren<Collider>();
+        animator.SetTrigger(arm);
+    }
+    void SetCanMoveTrue()
+    {
+        player.canMove = true;
+    }
+    void SetCanMoveFalse()
+    {
+        player.canMove = false;
+    }
+    void EquipWeapon()
+    {
+        inhandplace.SetActive(true);
+        hudController.WeaponIsReady(true);
+        audioSource.volume = AudioManager.battleEffects_volume;
+        audioSource.clip = audio_collection.get_sword;
+        audioSource.Play();
+        // backplace.SetActive(false);
+        // Show weapon and sounds
+    }
+    void HideWeapon()
+    {
+        inhandplace.SetActive(false);
+        hudController.WeaponIsReady(false);
+        // backplace.SetActive(true);
+        // Hide weapon and sounds
+    }
+    void PlayFootstepSound()
+    {
+        // Make audiomanager and add to it footsteps
+        if (audioSource == null)
+        {
+            if (GetComponent<AudioSource>() == null)
+                gameObject.AddComponent<AudioSource>();
+            else
+                audioSource = GetComponent<AudioSource>();
+        }
+        if (audio_collection != null)
+        {
+            audioSource.clip = audio_collection.footsteps_gravel.GetRandomItem();
+            audioSource.volume = AudioManager.footstep_volume;
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.Log("There are no any AudioCollection component");
+        }
+    }
+
+    // ------ Coroutines -------
+    IEnumerator DeathAnimation()
+    {
+        animator.SetTrigger(die);
+        yield return new WaitForSeconds(AFTER_DEATH_WAIT);
+        // Call method Death() from ai_controller
+
+        player.Death();
+    }
+}
